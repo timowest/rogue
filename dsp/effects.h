3b9c0f397a238eceb8913a02498720ea464d8147
Timo Westkämper <timo.westkamper@mysema.com>
2013-06-19 22:07:37 +0300

    #17 Add configure methods

--- dsp/effects.h
+++ dsp/effects.h
@@ -39,12 +39,21 @@ class Flanger {
 // Chorus
 
 class Chorus : Effect {
+    // conf
     float delay, feedback, rate, mod, wet;
 
     Flanger flanger1, flanger2;
     float lfo_phase = 0.0f;
 
   public:
+    void configure(float delay_, float feedback_, float rate_, float mod_, float wet_) {
+        delay = delay_;
+        feedback = feedback_;
+        rate = rate_;
+        mod = mod_;
+        wet = wet_;
+    }
+
     void reset();
     void process(float* l, float* r, int samples);
 };
@@ -67,12 +76,20 @@ class APDelay {
 };
 
 class Phaser : Effect {
+    // conf
     float lfo_rate, depth, feedback, wet;
 
     APDelay ap_l[6], ap_r[6];
     float lfo_phase = 0.0, out_l = 0.0, out_r;
 
   public:
+    void configure(float lfo_rate_, float depth_, float fb_, float wet_) {
+        lfo_rate = lfo_rate_;
+        depth = depth_;
+        feedback = fb_;
+        wet = wet_;
+    }
+
     void reset();
     void process(float* l, float* r, int samples);
 };
@@ -84,14 +101,21 @@ class Delay : Effect {
     static const int MAX_SIZE = 65536; //= (1 << 16);
     static const int MAX_MASK = MAX_SIZE - 1;
 
+    // conf
     float wet, delay, feedb, bpm = 0.0f;
 
-    float sample_rate, out_l, out_r;
+    float out_l, out_r;
     float buffer_l[MAX_SIZE], buffer_r[MAX_SIZE];
     int frames;
 
   public:
-    void setSamplerate(float rate) { sample_rate = rate; }
+    void configure(float wet_, float delay_, float feedb_, float bpm_) {
+        wet = wet_;
+        delay = delay_;
+        feedb = feedb_;
+        bpm = bpm_;
+    }
+
     void reset();
     void process(float* l, float* r, int samples);
 };
