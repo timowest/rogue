26f229bc4c3cd5b11597f9a833b02710301c2f95
Timo Westkämper <timo.westkamper@mysema.com>
2013-07-08 10:05:49 +0300

    Move gui creation to rogueGui class

--- src/qt/test.cpp
+++ src/qt/test.cpp
@@ -6,31 +6,107 @@
 #include <QFile>
 #include <QString>
 
-// browser
+class rogueGui : public QWidget {
 
-// main
+	// browser
 
-// effects
+	// main
 
-void createOscillators(QWidget* parent) {
-	QGridLayout* oscGrid = new QGridLayout(parent);
-	oscGrid->addWidget(new QGroupBox("1"), 1, 1);
-	oscGrid->addWidget(new QGroupBox("2"), 1, 2);
-	oscGrid->addWidget(new QGroupBox("3"), 2, 1);
-	oscGrid->addWidget(new QGroupBox("4"), 2, 2);
-}
+	QWidget* createEffects(QWidget* parent) {
+		QHBoxLayout* layout = new QHBoxLayout();
+		QTabWidget* tabs = new QTabWidget();
+		tabs->setTabPosition(QTabWidget::West);
+		tabs->addTab(new QGroupBox(), "C");
+		tabs->addTab(new QGroupBox(), "P");
+		tabs->addTab(new QGroupBox(), "D");
+		tabs->addTab(new QGroupBox(), "R");
+		layout->addWidget(tabs);
+		parent->setLayout(layout);
+		return parent;
+	}
 
-void createFilters(QWidget* parent) {
-	QGridLayout* filterGrid = new QGridLayout(parent);
-	filterGrid->addWidget(new QGroupBox("1"), 1, 1);
-	filterGrid->addWidget(new QGroupBox("2"), 2, 1);
-}
+	QWidget* createOscillators(QWidget* parent) {
+		QGridLayout* oscGrid = new QGridLayout();
+		oscGrid->addWidget(new QGroupBox("1"), 1, 1);
+		oscGrid->addWidget(new QGroupBox("2"), 1, 2);
+		oscGrid->addWidget(new QGroupBox("3"), 2, 1);
+		oscGrid->addWidget(new QGroupBox("4"), 2, 2);
+		parent->setLayout(oscGrid);
+		return parent;
+	}
 
-// envs
+	QWidget* createFilters(QWidget* parent) {
+		QGridLayout* filterGrid = new QGridLayout();
+		filterGrid->addWidget(new QGroupBox("1"), 1, 1);
+		filterGrid->addWidget(new QGroupBox("2"), 2, 1);
+		parent->setLayout(filterGrid);
+		return parent;
+	}
 
-// mod
+	QWidget* createEnvs(QWidget* parent) {
+		QHBoxLayout* layout = new QHBoxLayout();
+		QTabWidget* tabs = new QTabWidget();
+		tabs->setTabPosition(QTabWidget::West);
+		tabs->addTab(new QGroupBox(), "1");
+		tabs->addTab(new QGroupBox(), "2");
+		tabs->addTab(new QGroupBox(), "3");
+		tabs->addTab(new QGroupBox(), "4");
+		tabs->addTab(new QGroupBox(), "5");
+		layout->addWidget(tabs);
+	    parent->setLayout(layout);
+	    return parent;
+	}
 
-// lfos
+	QWidget* createMod(QWidget* parent) {
+		QHBoxLayout* layout = new QHBoxLayout();
+		QTabWidget* tabs = new QTabWidget();
+		tabs->setTabPosition(QTabWidget::West);
+		tabs->addTab(new QGroupBox(), "1");
+		tabs->addTab(new QGroupBox(), "2");
+		layout->addWidget(tabs);
+		parent->setLayout(layout);
+		return parent;
+	}
+
+	QWidget* createLfos(QWidget* parent) {
+		QHBoxLayout* layout = new QHBoxLayout();
+		QTabWidget* tabs = new QTabWidget();
+		tabs->setTabPosition(QTabWidget::West);
+		tabs->addTab(new QGroupBox(), "1");
+		tabs->addTab(new QGroupBox(), "2");
+		tabs->addTab(new QGroupBox(), "3");
+		layout->addWidget(tabs);
+		parent->setLayout(layout);
+		return parent;
+	}
+
+  public:
+
+	rogueGui(QWidget* parent = 0) : QWidget(parent) {
+		QWidget* browser = new QGroupBox("Browser");
+		QWidget* main = new QGroupBox("Main");
+	    QWidget* effects = createEffects(new QGroupBox("Effects"));
+	    QWidget* oscs = createOscillators(new QGroupBox("Oscillators"));
+	    QWidget* filters = createFilters(new QGroupBox("Filters"));
+	    QWidget* envs = createEnvs(new QGroupBox("Envelops"));
+	    QWidget* mod = createMod(new QGroupBox("Modulation"));
+	    QWidget* lfos = createLfos(new QGroupBox("LFOs"));
+
+	    // main grid
+	    QGridLayout* mainGrid = new QGridLayout(this);
+	    mainGrid->addWidget(browser, 1, 1);
+	    mainGrid->addWidget(main, 1, 2);
+	    mainGrid->addWidget(effects, 1, 3);
+	    mainGrid->addWidget(oscs, 2, 1, 2, 2);
+	    mainGrid->addWidget(filters, 2, 3, 2, 1);
+	    mainGrid->addWidget(envs, 4, 1);
+	    mainGrid->addWidget(mod, 4, 2);
+	    mainGrid->addWidget(lfos, 4, 3);
+	}
+
+
+};
+
 
 int main(int argc, char *argv[]) {
 	QFile file("src/qt/stylesheet.qss");
@@ -39,25 +115,7 @@ int main(int argc, char *argv[]) {
     QApplication app(argc, argv);
     app.setStyleSheet(styleSheet);
 
-    QGroupBox browser("Browser"), main("Main"), effects("Effects");
-    QGroupBox oscs("Oscillators"), filters("Filters");
-    QGroupBox envs("Envelopes"), mod("Modulation"), lfos("LFOs");
-
-    createOscillators(&oscs);
-    createFilters(&filters);
-
-    // main grid
-    QWidget window;
-    QGridLayout mainGrid(&window);
-    mainGrid.addWidget(&browser, 1, 1);
-    mainGrid.addWidget(&main, 1, 2);
-    mainGrid.addWidget(&effects, 1, 3);
-    mainGrid.addWidget(&oscs, 2, 1, 2, 2);
-    mainGrid.addWidget(&filters, 2, 3, 2, 1);
-    mainGrid.addWidget(&envs, 4, 1);
-    mainGrid.addWidget(&mod, 4, 2);
-    mainGrid.addWidget(&lfos, 4, 3);
-
+    rogueGui window;
     window.resize(900, 500);
     window.setWindowTitle("rogue");
     window.show();
