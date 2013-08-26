4996e97c4a7be32542819802664116c5a214f0aa
Timo Westkämper <timo.westkamper@mysema.com>
2013-08-26 23:09:52 +0300

    Use a loop instead of recursion #63

--- dsp/envelope.cpp
+++ dsp/envelope.cpp
@@ -36,76 +36,81 @@ void AHDSR::setAHDSR(float _a, float _h, float _d, float _s, float _r) {
 }
 
 float AHDSR::innerTick(int samples) {
-    if (state_ == PRE) {      // pre-delay
-        counter -= samples;
-        if (counter <= 0) {
-            state_ = A;
-            if (counter < 0) {
-                int left = -counter;
-                counter = 0;
-                return innerTick(left);
+    while (true) {
+        if (state_ == PRE) {      // pre-delay
+            counter -= samples;
+            if (counter <= 0) {
+                state_ = A;
+                if (counter < 0) {
+                    samples = -counter;
+                    counter = 0;
+                    continue;
+                }
             }
-        }
-    } else if (state_ == A) { // attack
-        last += samples * attackRate;
-        if (last >= 1.0f) {
-            int left = (last - 1.0) / attackRate;
-            if (holdSamples > 0.0f) {
-                state_ = H;
-                counter = holdSamples;
-                last = 1.0f;
-            } else {
+        } else if (state_ == A) { // attack
+            last += samples * attackRate;
+            if (last >= 1.0f) {
+                samples = (last - 1.0) / attackRate;
+                if (holdSamples > 0.0f) {
+                    state_ = H;
+                    counter = holdSamples;
+                    last = 1.0f;
+                } else {
+                    state_ = D;
+                    scale = sustain - attackTarget;
+                    offset = attackTarget;
+                    last = 0.0f;
+                }
+                if (samples > 0) {
+                    continue;
+                }
+            }
+        } else if (state_ == H) { // hold
+            counter -= samples;
+            if (counter <= 0) {
                 state_ = D;
                 scale = sustain - attackTarget;
                 offset = attackTarget;
                 last = 0.0f;
+                if (counter < 0) {
+                    samples = -counter;
+                    counter = 0;
+                    continue;
+                }
             }
-            if (left > 0) {
-                return innerTick(left);
-            }
-        }
-    } else if (state_ == H) { // hold
-        counter -= samples;
-        if (counter <= 0) {
-            state_ = D;
-            scale = sustain - attackTarget;
-            offset = attackTarget;
-            last = 0.0f;
-            if (counter < 0) {
-                int left = -counter;
-                counter = 0;
-                return innerTick(counter);
-            }
-        }
-    } else if (state_ == D) { // decay
-        last += samples * decayRate;
-        if (last >= 1.0f) {
-            int left = (last - 1.0) / decayRate;
-            state_ = S;
-            scale = -sustain;
-            offset = sustain;
-            last = 0.0f;
-            if (left > 0) {
-                return innerTick(left);
-            }
-        }
-    } else if (state_ == R) { // release
-        last += samples * releaseRate;
-        if (last >= 1.0f) {
-            int left = (last - 1.0) / releaseRate;
-            if (retrigger) {
-                last = 1.0;
-                on();
-            } else {
-                state_ = IDLE;
-                scale = offset = 0.0f;
+        } else if (state_ == D) { // decay
+            last += samples * decayRate;
+            if (last >= 1.0f) {
+                samples = (last - 1.0) / decayRate;
+                state_ = S;
+                scale = -sustain;
+                offset = sustain;
                 last = 0.0f;
+                if (samples > 0) {
+                    continue;
+                }
             }
-            if (left > 0) {
-                return innerTick(left);
+        } else if (state_ == R) { // release
+            last += samples * releaseRate;
+            if (last >= 1.0f) {
+                samples = (last - 1.0) / releaseRate;
+                if (retrigger) {
+                    last = 1.0;
+                    on();
+                } else {
+                    state_ = IDLE;
+                    scale = offset = 0.0f;
+                    last = 0.0f;
+                    break;
+                }
+                if (samples > 0) {
+                    continue;
+                }
             }
         }
+        break;
     }
+
     return last;
 }
 
